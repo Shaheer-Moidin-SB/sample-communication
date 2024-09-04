c57cce58eec0d99c0c8725bdce660492ef5941e6
Shaheer-Moidin-SB <moidinsb24@gmail.com>
2024-09-04 15:02:17 +0400

    Delete dist directory
    Delete dist directory

--- dist/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { createUserEvent } from './create-user.event';
-export declare class AppService {
-    getHello(): string;
-    handleUserCreated(data: createUserEvent): Promise<void>;
-    getNotificationData(payload: any): {};
-    fetchInvoiceEmailBody(data: any): Promise<string>;
-}
